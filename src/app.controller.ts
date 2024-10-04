a01cb487da74b48a8ef76b129bdb688437bf0ba4
kevin <bastokevindaniel@gmail.com>
2024-10-04 09:26:35 -0600

    feat: enable cors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Post()
   getHello(@Body() data){
     console.log(data)
     return this.appService.generateInvoice(data);
